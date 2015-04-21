f4f7de310da33457d0918d5681a33eb1e1fe53ff
RosaLuz <rosaluzzamorapeinado@gmail.com>
2015-04-21 11:13:58 -0700

    Añado Producto6

---
+++ Producto6/Proyectil.f90
@@ -0,0 +1,132 @@
+MODULE constantes
+IMPLICIT NONE
+REAL, PARAMETER :: rad=(4.0*ATAN(1.0))/180  
+REAL, PARAMETER :: pi=4.0*ATAN(1.0)
+INTEGER, PARAMETER :: puntos= 5000 
+
+
+REAL, PARAMETER :: rho = 1.29
+
+REAL, PARAMETER :: esfera = 0.47
+
+END MODULE constantes
+
+!-------------------------------------------------------------
+
+SUBROUTINE SinFriccion (xi,yi,vi,anguloi,xmaxsf,ymaxsf,tiemposf)
+USE constantes
+IMPLICIT NONE
+INTEGER :: I
+REAL, DIMENSION (1:puntos) :: x,y,t
+REAL :: xi, yi, vi, anguloi    
+REAL :: xmaxsf, ymaxsf, tiemposf  
+
+anguloi=anguloi*rad
+
+xmaxsf = xi+((vi*vi*SIN(2*anguloi))/(9.8))
+ymaxsf = yi+(((vi*vi)*(SIN(anguloi)*SIN(anguloi)))/(19.6))
+tiemposf = (2*vi*SIN(anguloi))/(9.806)
+
+OPEN (1, FILE="SinFriccion.dat")
+
+DO I=1, puntos, 1
+t(I)=FLOAT(I)*0.01
+x(I) = xi + (vi*COS(anguloi)*t(I))
+y(I) = yi + (vi*SIN(anguloi)*t(I))-(4.9*t(I)*t(I))
+WRITE (1,1001) x(I), y(I)
+1001 FORMAT (f11.5,f11.5)
+IF (y(I)<0) EXIT
+END DO
+
+CLOSE (1)
+
+END SUBROUTINE SinFriccion
+
+!----------------------------------------------------------------------------
+SUBROUTINE Friccion (xi,yi,vi,anguloi,xmaxf,ymaxf,tiempof)
+USE constantes
+IMPLICIT NONE
+INTEGER :: I 
+CHARACTER :: objeto
+REAL, DIMENSION (0:puntos) :: a,b,c,va,vb,pa,pb 
+REAL :: xi , yi , vi , anguloi    
+REAL :: xmaxf, ymaxf, tiempof  
+REAL :: ad, area, radio, cd, masa
+
+
+PRINT *, "Ingresa la masa de la esfera en kg"
+READ *, masa
+
+PRINT *, "Ingresa el radio de la esfera"
+READ *, radio
+area = pi*radio*radio
+cd = esfera
+
+
+a(0) = xi
+b(0) = yi
+va(0) = vi*COS(anguloi)
+vb(0) = vi*SIN(anguloi)
+ad = (0.5*rho*area*cd)/masa
+pa(0) = -ad*va(0)*va(0)
+pb(0) = 9.8-(ad*vb(0)*vb(0))
+c(0)=0
+
+OPEN (2, FILE="Friccion.dat")
+WRITE (2,1001) a(0),b(0)
+1001 FORMAT (f11.5,f11.5)
+
+DO I=0, puntos, 1
+  c(I+1) = c(I) + 0.01
+  va(I+1) = va(I)+pa(I)*c(I+1)  
+  vb(I+1) = vb(I)+pb(I)*c(I+1)
+  pa(I+1) = -ad*va(I)*va(I)
+  pb(I+1) = -9.8-(ad*va(I)*va(I))
+  a(I+1) = a(I)+va(I)*c(I+1)+(0.5*pa(I)*c(I+1)*c(I+1))
+  b(I+1) = b(I)+vb(I)*c(I+1)+(0.5*pb(I)*c(I+1)*c(I+1))
+  WRITE (2,*) a(I+1), b(I+1)
+  IF (b(I)<0) EXIT
+END DO
+
+CLOSE (2)
+
+xmaxf = a(I)
+ymaxf = MAXVAL(b)
+tiempof = c(I)*10.0
+
+END SUBROUTINE Friccion
+
+!------------------------------------------------------------------
+PROGRAM ProyectilConFriccion
+USE constantes
+IMPLICIT NONE
+REAL :: xi, yi, vi, anguloi
+REAL :: xmaxf, ymaxf, tiempof, xmaxsf, ymaxsf, tiemposf
+REAL :: error 
+
+PRINT *, "Ingresa los valores iniciales: x0, y0, v0, angulo0"
+READ *, xi, yi, vi, anguloi
+
+
+CALL SinFriccion (xi,yi,vi,anguloi,xmaxsf,ymaxsf,tiemposf)
+CALL Friccion (xi,yi,vi,anguloi,xmaxf,ymaxf,tiempof)
+
+error = ((xmaxsf-xmaxf)/xmaxf) * 100.0
+
+PRINT *, "Posición", xi, yi
+PRINT *, "Velocidad inicial", vi, "m/s"
+PRINT *, "Y un angulo de lanzamiento=", anguloi, "respecto a la horizontal"
+PRINT *, "----------------------------------------------"
+PRINT *, "SIN FRICCIÓN"
+PRINT *, "Tiempo total de vuelo", tiemposf, "segundos"
+PRINT *, "Altura máxima", ymaxsf, "metros"
+PRINT *, "Alcance horizontal máximo", xmaxsf, "metros"
+PRINT *, "----------------------------------------------"
+PRINT *, "CON FRICCIÓN"
+PRINT *, "Tiempo total de vuelo", tiempof, "segundos"
+PRINT *, "Altura máxima", ymaxf, "metros"
+PRINT *, "Alcance horizontal máximo", xmaxf, "metros"
+PRINT *, "----------------------------------------------"
+PRINT *, "Error sin considerar la fricción del aire:", error, "%"
+
+END PROGRAM ProyectilConFriccion
---
+++ Producto6/constantes.mod
@@ -0,0 +1,44 @@
+GFORTRAN module version '10' created from Proyectil.f90
+MD5:91e63a17f1aacea72beae5443eb5e432 -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () () ()
+() () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 '__convert_i4_r4' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 FUNCTION ELEMENTAL PURE) (REAL 4 0 0 0
+REAL ()) 0 0 () () 2 () () () 0 0)
+3 'atan' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0 FUNCTION) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 3 ()
+() () 0 0)
+4 'constantes' 'constantes' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0
+0)
+5 'esfera' 'constantes' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 4 0 0 0 REAL ()) 0 0 () (CONSTANT (
+REAL 4 0 0 0 REAL ()) 0 '0.7851eb8@0') () 0 () () () 0 0)
+6 'pi' 'constantes' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (REAL 4 0 0 0 REAL ()) 0 0 () (CONSTANT (REAL 4 0 0 0
+REAL ()) 0 '0.3243f6c@1') () 0 () () () 0 0)
+7 'puntos' 'constantes' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '5000') () 0 () () () 0 0)
+8 'rad' 'constantes' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 4 0 0 0 REAL ()) 0 0 () (CONSTANT (
+REAL 4 0 0 0 REAL ()) 0 '0.477d1a8@-1') () 0 () () () 0 0)
+9 'rho' 'constantes' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 4 0 0 0 REAL ()) 0 0 () (CONSTANT (
+REAL 4 0 0 0 REAL ()) 0 '0.14a3d70@1') () 0 () () () 0 0)
+)
+
+('__convert_i4_r4' 0 2 'atan' 0 3 'constantes' 0 4 'esfera' 0 5 'pi' 0 6
+'puntos' 0 7 'rad' 0 8 'rho' 0 9)
